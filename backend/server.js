b54aed14ad29496fe0b8eac92bb055235fc5dae7
Mohammad Arhan <mohammadarhan837@gmail.com>
2026-06-30 21:13:14 +0530

    Update CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import noteRoutes from "./routes/noteRoutes.js";
-import problemRoutes from './routes/problemRoutes.js';
-import applicationRoutes from './routes/applicationRoutes.js';
-import dashboardRoutes from './routes/dashboardRoutes.js';
-
+import problemRoutes from "./routes/problemRoutes.js";
+import applicationRoutes from "./routes/applicationRoutes.js";
+import dashboardRoutes from "./routes/dashboardRoutes.js";
 
 dotenv.config();
 
@@ -16,25 +16,22 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(express.json());
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://preppilot-vert-seven.vercel.app",
-    ],
-    credentials: true,
-  })
-);
-
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://preppilot-vert-seven.vercel.app",
+  ],
+  credentials: true,
+}));
 
 app.use(express.json());
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
 app.use("/api/problems", problemRoutes);
 app.use("/api/applications", applicationRoutes);
-app.use('/api/dashboard', dashboardRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
 app.get("/", (req, res) => {
   res.send("PrepPilot API Running");
