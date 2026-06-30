e161b5e2d9c472e714610e4b65fa98bdcc5edccb
Mohammad Arhan <mohammadarhan837@gmail.com>
2026-06-30 21:07:41 +0530

    Fix CORS  for Vercel

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://preppilot-vert-seven.vercel.app/",
+      "https://preppilot-vert-seven.vercel.app",
     ],
     credentials: true,
   })
